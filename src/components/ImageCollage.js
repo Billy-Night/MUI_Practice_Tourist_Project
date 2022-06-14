dfb79e73f3512a4d691e437cf58bb7660327fe5c
Billy <billy.m.nightingale@gmail.com>
2022-06-14 22:27:59 +0200

    resolved error

--- src/components/ImageCollage.js
+++ src/components/ImageCollage.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
+function srcset(image, size, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
     srcSet: `${image}?w=${size * cols}&h=${
